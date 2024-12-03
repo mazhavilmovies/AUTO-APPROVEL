ea1a3e0445e3043a0e980560efa740c530ef1de8
MN - TG <mntg3509@gmail.com>
2024-12-03 10:33:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ API_HASH = os.getenv("API_HASH")
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
 JOIN_REQUEST_LOG_CHANNEL_ID = int(os.getenv("JOIN_REQUEST_LOG_CHANNEL_ID"))
 MONGO_URI = os.getenv("MONGO_URI")
+AUTH_CHANNEL = '<your_auth_channel_id_here>'
+REQ_CHANNEL = '<your_req_channel_id_here>'

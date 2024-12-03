09ed88c6830cca9f9691f8304d9391dcebd6f691
MN - TG <mntg3509@gmail.com>
2024-12-03 09:52:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
+JOIN_REQUEST_LOG_CHANNEL_ID = int(os.getenv("JOIN_REQUEST_LOG_CHANNEL_ID"))
+MONGO_URI = os.getenv("MONGO_URI")
